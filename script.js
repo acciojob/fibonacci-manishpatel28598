9b4448f6f7ef39f89e5fea85920ea55d931eb7d6
Manish kumar <119885517+manishpatel28598@users.noreply.github.com>
2024-02-07 19:41:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function fibonacci(num) {
 // your code here
+	let a = 0;
+	let b = 1;
+	let c = a+b;
+	for (let index = 0; index <= num; index++) {
+		a=b;
+		b=c;
+		c = a+b;
+	}
+	return c;
 }
 
 module.exports = fibonacci;
