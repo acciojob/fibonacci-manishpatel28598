666911ad1e46211f526a7d173d011e02dbcaba8d
Manish kumar <119885517+manishpatel28598@users.noreply.github.com>
2024-02-10 22:53:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function fibonacci(num) {
 	let a = 0;
 	let b = 1;
 	let c = a+b;
-	for (let index = 0; index <= num; index++) {
+	for (let index = 2; index <= num; index++) {
 		a=b;
 		b=c;
 		c = a+b;
